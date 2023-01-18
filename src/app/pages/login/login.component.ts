23761237cf5ca59a5eb16bfdee013a3cc32c59e5
Leonel Huachaca <leonel.huachacav@gmail.com>
2023-01-18 10:01:37 -0500

    add  validate login to guard

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AbstractControl, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,14 +11,25 @@ export class LoginComponent {
   regexEmail =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private router: Router) {}
 
   formLogin = this.fb.group({
-    email: ['test@test.com', Validators.pattern(this.regexEmail)],
-    password: ['123456', Validators.minLength(6)],
+    email: [
+      'test@test.com',
+      [Validators.required, Validators.pattern(this.regexEmail)],
+    ],
+    password: ['123456', [Validators.required, Validators.minLength(6)]],
   });
 
-  login() {}
+  login() {
+    if (
+      this.form['email'].value === 'test@test.com' &&
+      this.form['password'].value === '123456'
+    ) {
+      localStorage.setItem('isLogin', 'true');
+      this.router.navigate(['/home']);
+    }
+  }
 
   get form(): { [key: string]: AbstractControl } {
     return this.formLogin.controls;
